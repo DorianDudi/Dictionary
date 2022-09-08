f8c1468f5f961226871d3c34778dc7c8c60ad0fb
Dorian-DV <ddorianvlad@yahoo.ro>
2022-09-08 21:30:01 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,21 +3,20 @@ const dictionary = [];
 function addWord() {
 	let inputField = document.getElementById("addField");
 	if(inputField.value != "") {
-  	dictionary.push(inputField.value);
-    alert(inputField.value + " has been added to dictionary");
-    inputField.value = "";
-    listDictionary();
-  } else {
-  	alert("Please type the word you want to add");
-  }
+		dictionary.push(inputField.value);
+		alert(inputField.value + " has been added to dictionary");
+		inputField.value = "";
+	} else {
+		alert("Please type the word you want to add");
+	}
 }
 
 function searchForWord() {
 	let searchField = document.getElementById("searchField");
-  if(dictionary.includes(searchField.value)) {
-  	alert("Word found in Dictionary!");
-  } else {
-  	alert("Word not found in Dictionary!");
-  }
-  searchField.value = "";
+	if(dictionary.includes(searchField.value)) {
+		alert("Word found in Dictionary!");
+	} else {
+		alert("Word not found in Dictionary!");
+	}
+	searchField.value = "";
 }
